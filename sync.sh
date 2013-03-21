69a7619124e1be07a91fbd01f71e13cf679d7450
Ulrich Werner <ulrichwern@gmail.com>
2013-03-21 12:42:01 -0500

    Start using vundle, add psqlrc

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc gitconfig vim vimrc inputrc fonts ctags tmux.conf"    # list of files/folders to symlink in homedir
+files="bashrc gitconfig vim vimrc inputrc fonts ctags tmux.conf psqlrc"    # list of files/folders to symlink in homedir
 
 
 # create dotfiles_old in homedir
@@ -29,27 +29,8 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-# download all vim/bundles from their github repos
-cd $dir
-git submodule update --init
-
-##########################
-# Install Command-T plugin
-# To avoid using ruby's bundler, I have to source the vimball then configure/make the C extension
-# 
-# If you get errors, make the C extension with the same
-# version of ruby vim was installed with:
-# vim --version
-# ruby --version
-#########################
-cd $dir
-# set nomore lets vim quit out after sourcing the vimball
-vim -c 'set nomore' -c 'so %'  -c 'q!' $dir/vim/vimball/command-t/command-t-1.3.1.vba
-# make the c extension so command-t works + is faster
-cd ~/.vim/ruby/command-t 
-/usr/bin/ruby185 extconf.rb 
-make 
-cd $dir
+# Install vim vundle bundles
+vim +BundleInstall +qall
 
 echo ""
 echo "+==================================+"
